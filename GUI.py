Upgraded neighbors computation panel. Fixed control in 2D view. Added z vel control

GUI.py
@@ -40,28 +40,30 @@ class myApp(tk.Frame):
         #==================================#
         try:
             with open(CONFIG_FILENAME) as f:
-                config = json.load(f)
+                self.app_config = json.load(f)
         except FileNotFoundError:
-            config = {}
+            self.app_config = {}
         # Initialize app variables with json values or defaults
-        self.var_drone_count = tk.IntVar(value=config.get('drone_count', DEFAULT_NB_DRONES))
-        self.var_neighbor_count = tk.IntVar(value=config.get('neighbor_count', DEFAULT_NB_DRONES-1))
-        self.var_swarm_spread = tk.DoubleVar(value=config.get('swarm_spread', 1.0))
-        self.var_noise_pos = tk.DoubleVar(value=config.get('noise_pos', 0.025))
-        self.var_noise_orient = tk.DoubleVar(value=config.get('noise_orient', 0.025))
-        self.var_noise_heading = tk.DoubleVar(value=config.get('noise_heading', 0.0))
-        self.var_delta = tk.DoubleVar(value=config.get('delta', 0.0))
-        self.var_coh = tk.DoubleVar(value=config.get('r_coh', 0.0))
-        self.var_vref = tk.DoubleVar(value=config.get('vref', 0.0))
-        self.var_a = tk.DoubleVar(value=config.get('a', 0.0))
-        self.var_b = tk.DoubleVar(value=config.get('b', 0.0))
-        self.var_c = tk.DoubleVar(value=config.get('c', 0.0))
-        self.var_z_offset = tk.DoubleVar(value=config.get('z_offset', 10.0))
-        self.var_target = tk.StringVar(value='')
-        self.var_neighbor_sampling = tk.IntVar(value=config.get('neighbor_sampling', 1))
-        self.spawn_box = config.get('spawn_box', [0,0,10,5,5,5])
-        self.cmd_yaw = config.get('cmd_yaw', 0.5)
-        self.cmd_vel = config.get('cmd_vel', 0.5)
+        self.var_drone_count = tk.IntVar(value=self.app_config.get('drone_count', DEFAULT_NB_DRONES))
+        self.var_neighbor_count = tk.IntVar(value=self.app_config['neighbors'].get('count', DEFAULT_NB_DRONES-1))
+        self.var_neighbor_radius = tk.DoubleVar(value=self.app_config['neighbors'].get('sensing_range', 1.0))
+        self.var_neighbor_r_agent = tk.DoubleVar(value=self.app_config['neighbors'].get('r_agent', 0.01))
+        self.var_swarm_spread = tk.DoubleVar(value=self.app_config.get('swarm_spread', 1.0))
+        self.var_noise_pos = tk.DoubleVar(value=self.app_config.get('noise_pos', 0.025))
+        self.var_noise_orient = tk.DoubleVar(value=self.app_config.get('noise_orient', 0.025))
+        self.var_noise_heading = tk.DoubleVar(value=self.app_config.get('noise_heading', 0.0))
+        self.var_delta = tk.DoubleVar(value=self.app_config.get('delta', 0.0))
+        self.var_coh = tk.DoubleVar(value=self.app_config.get('r_coh', 0.0))
+        self.var_vref = tk.DoubleVar(value=self.app_config.get('vref', 0.0))
+        self.var_a = tk.DoubleVar(value=self.app_config.get('a', 0.0))
+        self.var_b = tk.DoubleVar(value=self.app_config.get('b', 0.0))
+        self.var_c = tk.DoubleVar(value=self.app_config.get('c', 0.0))
+        self.var_z_offset = tk.DoubleVar(value=self.app_config.get('z_offset', 10.0))
+        self.var_agent = tk.StringVar(value='')
+        self.var_neighbor_sampling = tk.IntVar(value=self.app_config['neighbors'].get('sampling', 1))
+        self.spawn_box = self.app_config.get('spawn_box', [0,0,10,5,5,5])
+        self.cmd_yaw = self.app_config.get('cmd_yaw', 0.5)
+        self.cmd_vel = self.app_config.get('cmd_vel', 0.5)
 
         #==================================#
         # APP VARIABLES TRACKING  #
@@ -78,6 +80,8 @@ class myApp(tk.Frame):
         self.var_noise_orient.trace_add('write', self.noise_changed_callback)
         self.var_noise_heading.trace_add('write', self.noise_changed_callback)
         self.var_neighbor_sampling.trace_add('write', self._set_neighbors_algo_params)
+        self.var_neighbor_radius.trace_add('write', self._set_neighbors_algo_params)
+        self.var_neighbor_r_agent.trace_add('write', self._set_neighbors_algo_params)
         #==================================#
         #       APP INITIALIZATION         #
         #==================================#
@@ -133,20 +137,23 @@ class myApp(tk.Frame):
         # Neighbors
         self.panel_neighbors = tk.Frame(self.panel_params, borderwidth=2, relief='ridge')
         self.panel_neighbors.grid(column=0, row=1, columnspan=3, rowspan=1, sticky='NWES', padx=0, pady=5)
-        self.panel_neighbors.grid_columnconfigure((0,1,2,3,4), weight=1)
+        self.panel_neighbors.grid_columnconfigure((0,1,2,3,4,5), weight=1)
         self.panel_neighbors.grid_rowconfigure(0, weight=1)
         self.label_neighbors = ttk.Label(self.panel_neighbors, anchor='w', text="Neighbors: ")
         self.label_neighbors.grid(column=0,row=0, sticky='NEWS')
         self.listbox_neighbors_algo = ttk.Combobox(self.panel_neighbors, values=["Eucledian", "Topological", "Voronoi", "Visual LoS"])
-        self.listbox_neighbors_algo.set("Topological")
+        self.listbox_neighbors_algo.set(self.app_config['neighbors'].get('metric', 'Topological'))
         self.listbox_neighbors_algo.grid(row=0,column=1,sticky='we', padx=5)
-        self.listbox_neighbors_algo.bind("<<ComboboxSelected>>", lambda e: self._set_neighbors_algo_params())
+        self.listbox_neighbors_algo.bind("<<ComboboxSelected>>", lambda e: self._update_neighbors_panel_components())
         self.label_neighbors_algo_param = ttk.Label(self.panel_neighbors, anchor='e', text="# ")
         self.label_neighbors_algo_param.grid(column=2,row=0,sticky='NEWS')
-        self.spinner_neighbors = ttk.Spinbox(self.panel_neighbors, increment=1,from_=0, to=self.var_drone_count.get(), textvariable=self.var_neighbor_count)
+        self.spinner_neighbors = ttk.Spinbox(self.panel_neighbors, increment=1,from_=0, to=self.var_drone_count.get()-1, textvariable=self.var_neighbor_count)
         self.spinner_neighbors.grid(row=0,column=3,sticky='w', padx=5)
-        self.btn_apply_all_neighbors = ttk.Button(self.panel_neighbors, text="Apply to all", command=lambda e: self._set_neighbors_algo_params(apply_to_all=True))
-        self.btn_apply_all_neighbors.grid(row=0,column=4,sticky='wens', padx=5, pady=10)
+        self.label_spinner_r_agent = ttk.Label(self.panel_neighbors, anchor='e', text="r_agent: ")
+        self.spinner_r_agent = ttk.Spinbox(self.panel_neighbors, increment=0.01, from_=0, to=1, textvariable=self.var_neighbor_r_agent)
+        # Disable all components in the neighbors panel
+        for child in self.panel_neighbors.winfo_children():
+            child.config(state='disabled')
 
         # Swarm spread
         self.label_swarm_spread = ttk.Label(self.panel_params, anchor='w', text="Swarm spread (r): ")
@@ -210,6 +217,8 @@ class myApp(tk.Frame):
         self.listbox_noise_type.set("None")
         self.listbox_noise_type.grid(column=1,row=0, sticky='we', padx=5)
         self.listbox_noise_type.bind("<<ComboboxSelected>>", lambda e: self.noise_changed_callback())
+        self.btn_apply_noise_all = ttk.Button(self.pnael_noise, text="Apply to all", command=self.btn_apply_all_callback)
+        self.btn_apply_noise_all.grid(column=3,row=0, columnspan=2, sticky='NEWS', pady=10)
         self.label_noise_pos = ttk.Label(self.pnael_noise, anchor='e', text="Dist:", justify='right')
         self.label_noise_pos.grid(column=0,row=1, sticky='NEWS')
         self.spinner_noise_pos = ttk.Spinbox(self.pnael_noise, increment=0.01, from_=0, to=1, textvariable=self.var_noise_pos)
@@ -226,7 +235,7 @@ class myApp(tk.Frame):
         # Target
         self.label_target = ttk.Label(self.panel_params, anchor='w', text="Target: ")
         self.label_target.grid(column=0,row=8, sticky='NEWS')
-        self.textbox_target = ttk.Entry(self.panel_params, textvariable=self.var_target, font=font.Font(size=10))
+        self.textbox_target = ttk.Entry(self.panel_params, textvariable=self.var_agent, font=font.Font(size=10))
         self.textbox_target.grid(column=1, row=8, sticky='WE', padx=5)
         self.label_target_format = ttk.Label(self.panel_params, anchor='w', text="(#.#;#.#;#.#) or empty", justify='left', font=font.Font(size=10))
         self.label_target_format.grid(column=2,row=8, sticky='NEWS')
@@ -268,7 +277,7 @@ class myApp(tk.Frame):
         self.spinner_neighbor_sampling = ttk.Spinbox(self.panel_sim_neighbors, increment=1, from_=1, to=100, textvariable=self.var_neighbor_sampling)
         self.spinner_neighbor_sampling.grid(column=1, row=0, sticky='W', padx=5)
         self.listbox_neighbors_select = ttk.Combobox(self.panel_sim_neighbors, values=["None", "Selected", "All"])
-        self.listbox_neighbors_select.set("None")
+        self.listbox_neighbors_select.set(self.app_config['neighbors'].get('computation', 'None'))
         self.listbox_neighbors_select.bind("<<ComboboxSelected>>", lambda e: self._set_neighbors_algo_params())
         self.listbox_neighbors_select.grid(column=2, row=0, sticky='WE', padx=5)
 
@@ -280,12 +289,11 @@ class myApp(tk.Frame):
     def _initialize_simulation(self):
         # Retrieve all necessary parameters from app widgets
         nb_drones = self.var_drone_count.get()
-        swarm_spread = self.slider_spread.get()
         noise = {'type': self.listbox_noise_type.get(), 'param_pos': self.var_noise_pos.get(), 'param_heading': self.var_noise_orient.get()}
-        if self.var_target.get() == '':
+        if self.var_agent.get() == '':
             target = None
         else:
-            target_numbers = self.var_target.get().split(';')
+            target_numbers = self.var_agent.get().split(';')
             target = np.asarray(target_numbers, dtype=float)
 
         self.swarm = Swarm(count=nb_drones, box=self.spawn_box, noise=noise, migration_point=target)
@@ -305,6 +313,10 @@ class myApp(tk.Frame):
         self.btn_2D_view.config(state='normal')
         self.btn_simulate.config(state='normal')
         self.btn_center.config(state='normal')
+        # Enable all components in the neighbors panel
+        for child in self.panel_neighbors.winfo_children():
+            child.config(state='normal')
+        self._update_neighbors_panel_components()
 
     def _set_swarm_algo_params(self, *args):
         if self.swarm is None:
@@ -322,7 +334,38 @@ class myApp(tk.Frame):
         except Exception as e:
             print("Error setting swarm algo params: {0}".format(e))
 
-    def _set_neighbors_algo_params(self, apply_to_all=False, *args):
+    def _update_neighbors_panel_components(self):
+        current_algo = self.listbox_neighbors_algo.get()
+        self.spinner_neighbors.config(state='normal')
+        if self.label_spinner_r_agent in self.panel_neighbors.winfo_children():
+            self.label_spinner_r_agent.grid_forget()
+            self.spinner_r_agent.grid_forget()
+        match current_algo:
+            case 'Eucledian':
+                self.label_neighbors_algo_param.config(text="radius")
+                default_val = self.app_config['neighbors'].get('sensing_range', 1.0)
+                current_val = self.swarm.get_neighbor_metric('sensing_range', default_val)
+                self.var_neighbor_radius.set(current_val)
+                self.spinner_neighbors.config(from_=0, to=100, increment=0.1, textvariable=self.var_neighbor_radius)
+            case 'Topological':
+                self.label_neighbors_algo_param.config(text="# ")
+                default_val = self.app_config['neighbors'].get('count', 1)
+                current_val = self.swarm.get_neighbor_metric('count', default_val)
+                self.var_neighbor_count.set(current_val)
+                self.spinner_neighbors.config(from_=0, to=self.var_drone_count.get()-1, increment=1, textvariable=self.var_neighbor_count)
+            case 'Voronoi':
+                self.label_neighbors_algo_param.config(text="")
+                self.spinner_neighbors.config(state='disabled')
+            case 'Visual LoS':
+                self.label_neighbors_algo_param.config(text="radius ")
+                default_val = self.app_config['neighbors'].get('sensing_range', 1.0)
+                self.var_neighbor_radius.set(self.swarm.get_neighbor_metric('sensing_range', default_val))
+                self.spinner_neighbors.config(from_=0, to=100, increment=0.1, textvariable=self.var_neighbor_radius)
+                self.label_spinner_r_agent.grid(column=4,row=0,sticky='NEWS')
+                self.spinner_r_agent.grid(column=5,row=0,sticky='W')
+        self._set_neighbors_algo_params()
+    
+    def _set_neighbors_algo_params(self, *args):
         if self.swarm is None:
             return
         try:
@@ -330,8 +373,10 @@ class myApp(tk.Frame):
                 'computation': self.listbox_neighbors_select.get(),
                 'metric': self.listbox_neighbors_algo.get(),
                 'sampling': self.var_neighbor_sampling.get(),
-                'metric_specific': {'nb_neighbors': self.var_neighbor_count.get()}
-            }, apply_to_all)
+                'count' : self.var_neighbor_count.get(),
+                'sensing_range': self.var_neighbor_radius.get(),
+                'r_agent': self.var_neighbor_r_agent.get()
+            })
         except Exception as e:
             print("Error setting neighbors algo params: {0}".format(e))
 
@@ -342,6 +387,9 @@ class myApp(tk.Frame):
         self.btn_center.config(state='disabled')
         self.btn_simulate.config(state='disabled')
         self.btn_2D_view.config(state='disabled')
+        # Disable all components in the neighbors panel
+        for child in self.panel_neighbors.winfo_children():
+            child.config(state='disabled')
         try:
             self.sim.stop()
             self.sim = None
@@ -372,8 +420,12 @@ class myApp(tk.Frame):
             self.window_2d.title("2D Swarm viewer")
             self.window_2d.geometry('1000x800')
             self.renderer2D = Renderer2D(self.window_2d, self.swarm)
+            self.window_2d.bind("<KeyPress>", self.key_press_callback)
+            self.window_2d.bind("<KeyRelease>", self.key_release_callback)
         self.window_2d.deiconify()
 
+    def btn_apply_all_callback(self):
+        pass
 
     def noise_changed_callback(self, *args):
         if self.listbox_noise_type.get() == 'None':
@@ -409,6 +461,10 @@ class myApp(tk.Frame):
                 target_vel[1] = self.var_vref.get()
             case 'd':
                 target_vel[1] = -self.var_vref.get()
+            case 'Q':
+                target_vel[2] = self.cmd_vel
+            case 'E':
+                target_vel[2] = -self.cmd_vel
         self.swarm.set_cmd_velocity(target_vel)   
 
 
@@ -417,18 +473,14 @@ class myApp(tk.Frame):
             return
         target_vel = self.swarm.get_cmd_velocity()
         match event.char:
-            case 'q':
+            case 'q' | 'e':
                 self.swarm.set_cmd_ang_rates(np.array([0.0,0.0,0.0]))
-            case 'e':
-                self.swarm.set_cmd_ang_rates(np.array([0.0,0.0,0.0]))
-            case 'w':
+            case 'w' | 's':
                 target_vel[0] = 0
-            case 's':
-                target_vel[0] = 0
-            case 'a':
+            case 'a' | 'd':
                 target_vel[1] = 0
-            case 'd':
-                target_vel[1] = 0
+            case 'Q' | 'E':
+                target_vel[2] = 0
         self.swarm.set_cmd_velocity(target_vel)    
 
     def app_closing(self):

drone.py
@@ -52,10 +52,10 @@ class Drone:
             case "Eucledian":
                 sensing_range = metric_data.get('sensing_range', DEFAULT_RANGE_SENSING)
                 distances = np.array([np.linalg.norm(members[i].pos - self.pos) for i in poss_neighbors])
-                indices = np.nonzero(distances < sensing_range)
+                indices = np.nonzero(distances < sensing_range)[0]
                 self.neighbors = [DroneNeighbor(i, distances[j], (members[i].pos - self.pos) / distances[j], members[i].angles) for i,j in zip(poss_neighbors[indices], indices)]
             case "Topological":
-                nb = metric_data.get('nb_neighbors', DEFAULT_NB_NEIGHBORS)
+                nb = metric_data.get('count', DEFAULT_NB_NEIGHBORS)
                 distances = np.array([np.linalg.norm(members[i].pos - self.pos) for i in poss_neighbors])
                 indices = np.argsort(distances)[:nb]
                 self.neighbors = [DroneNeighbor(i, self._apply_noise(distances[j], noise, 'param_pos'), 
@@ -74,8 +74,8 @@ class Drone:
                 r_agent = metric_data.get('r_agent', 0.05)
                 # Keep neighbors that are within the sensing range
                 distances = np.array([np.linalg.norm(members[i].pos - self.pos) for i in poss_neighbors])
-                distances = distances[distances < sensing_range]
                 poss_neighbors = poss_neighbors[distances < sensing_range]
+                distances = distances[distances < sensing_range]
                 # Get headings and distances to all neighbors
                 headings = np.array([(members[i].pos - self.pos)/distances[j] for i,j in zip(poss_neighbors, range(len(poss_neighbors)))])
                 indices = np.argsort(distances)
@@ -84,7 +84,7 @@ class Drone:
                     n_index = poss_neighbors[indices[0]]
                     self.neighbors.append(DroneNeighbor(n_index, self._apply_noise(distances[indices[0]], noise, 'param_pos'), 
                                                         self._apply_noise(headings[indices[0]], noise, 'param_pos'), 
-                                                        self._apply_noise(members[n_index].angles), noise, 'param_heading'))
+                                                        self._apply_noise(members[n_index].angles, noise, 'param_heading')))
                     # Check if the neighbor is within the field of view
                     d_ij = distances[indices[0]]
                     u_ij = headings[indices[0]]

renderer.py
@@ -197,48 +197,50 @@ class Renderer2D():
         swarm_states = data[0]
         neighbors = data[1]
         selected_drone = data[2]
-        # Plot the drones as points
-        colors = ['#0000FFFF']*self._swarm_ref.count
-        colors[selected_drone] = '#80ff00'
-        self.artists["scatter_xy"].set(offsets=swarm_states[:,:2], color=colors)
-        self.artists["scatter_xz"].set(offsets=swarm_states[:,(0,2)], color=colors)
-        self.artists["scatter_yz"].set(offsets=swarm_states[:,1:3], color=colors)
-        # Plot the heading as arrows
-        if np.sum(np.abs(np.concatenate((swarm_states[:,-3], swarm_states[:,-2])))) > 0:
-            self.artists["quiver_xy"] = self.ax[0].quiver(swarm_states[:,0],swarm_states[:,1],swarm_states[:,-3],swarm_states[:,-2],width=0.005, animated=True)
-        if np.sum(np.abs(np.concatenate((swarm_states[:,-3], swarm_states[:,-1])))) > 0:  
-            self.artists["quiver_xz"] = self.ax[1].quiver(swarm_states[:,0],swarm_states[:,2],swarm_states[:,-3],swarm_states[:,-1],width=0.005, animated=True)
-        if np.sum(np.abs(np.concatenate((swarm_states[:,-2], swarm_states[:,-1])))) > 0:
-            self.artists["quiver_yz"] = self.ax[2].quiver(swarm_states[:,1],swarm_states[:,2],swarm_states[:,-2],swarm_states[:,-1],width=0.005, animated=True)
-        # Plot single drone + neighbors
-        # Apply needed rotations with respect to body frame
-        t_x, t_y = swarm_states[selected_drone,:2]
-        points = np.array([[-2*self.viewing_radius, 2*self.viewing_radius, 0, 0], 
-                            [0, 0, 2*-self.viewing_radius, 2*self.viewing_radius]])
-        R_2D = np.array([[swarm_states[selected_drone,-3], -swarm_states[selected_drone,-2]],
-                        [swarm_states[selected_drone,-2], swarm_states[selected_drone,-3]]])
-        r_points = R_2D @ points
-        self.artists["h_dashed"].set_data(r_points[0,:2], r_points[1,:2])
-        self.artists["v_dashed"].set_data(r_points[0,2:4], r_points[1,2:4])
-        colors = ['#FF0000A0']*(len(neighbors) + 1)
-        colors[-1] = '#80ff00'
-        sizes = [60]*(len(neighbors) + 1)
-        sizes[-1] = 150
-        if len(neighbors) > 0:
-            n_data = np.vstack([n.get_state()[0,0:2] for n in neighbors])
-        else:
-            n_data = np.array([0,0])
-        self.artists["scatter_single"].set(offsets=np.vstack((n_data, [0,0])), color=colors, sizes=sizes)
-        # Plot heading of estimated neighbors
-        if len(neighbors) > 0:
-            n_data2 = np.vstack([n.get_state()[3,0:2] for n in neighbors])
-            self.artists["quiver_n"] = self.ax[3].quiver(n_data[:,0],n_data[:,1],n_data2[:,0],n_data2[:,1],width=0.005, animated=True)
-        # Plot heading of the selected drone
-        scale = self.viewing_radius / 5.0
-        self.artists["arrow_single"] = self.ax[3].arrow(0, 0, swarm_states[selected_drone,-3], swarm_states[selected_drone,-2], width=scale*0.075, head_width=scale*0.3, head_length=scale*0.25, fc='g', ec='g')
-        # Plot swarm
-        self.artists["scatter_swarm"].set(offsets=[swarm_states[i,:2] - np.array([t_x,t_y]) for i in range(self._swarm_ref.count) if i != selected_drone])
-
+        try:
+            # Plot the drones as points
+            colors = ['#0000FFFF']*self._swarm_ref.count
+            colors[selected_drone] = '#80ff00'
+            self.artists["scatter_xy"].set(offsets=swarm_states[:,:2], color=colors)
+            self.artists["scatter_xz"].set(offsets=swarm_states[:,(0,2)], color=colors)
+            self.artists["scatter_yz"].set(offsets=swarm_states[:,1:3], color=colors)
+            # Plot the heading as arrows
+            if np.sum(np.abs(np.concatenate((swarm_states[:,-3], swarm_states[:,-2])))) > 0:
+                self.artists["quiver_xy"] = self.ax[0].quiver(swarm_states[:,0],swarm_states[:,1],swarm_states[:,-3],swarm_states[:,-2],width=0.005, animated=True)
+            if np.sum(np.abs(np.concatenate((swarm_states[:,-3], swarm_states[:,-1])))) > 0:  
+                self.artists["quiver_xz"] = self.ax[1].quiver(swarm_states[:,0],swarm_states[:,2],swarm_states[:,-3],swarm_states[:,-1],width=0.005, animated=True)
+            if np.sum(np.abs(np.concatenate((swarm_states[:,-2], swarm_states[:,-1])))) > 0:
+                self.artists["quiver_yz"] = self.ax[2].quiver(swarm_states[:,1],swarm_states[:,2],swarm_states[:,-2],swarm_states[:,-1],width=0.005, animated=True)
+            # Plot single drone + neighbors
+            # Apply needed rotations with respect to body frame
+            t_x, t_y = swarm_states[selected_drone,:2]
+            points = np.array([[-2*self.viewing_radius, 2*self.viewing_radius, 0, 0], 
+                                [0, 0, 2*-self.viewing_radius, 2*self.viewing_radius]])
+            R_2D = np.array([[swarm_states[selected_drone,-3], -swarm_states[selected_drone,-2]],
+                            [swarm_states[selected_drone,-2], swarm_states[selected_drone,-3]]])
+            r_points = R_2D @ points
+            self.artists["h_dashed"].set_data(r_points[0,:2], r_points[1,:2])
+            self.artists["v_dashed"].set_data(r_points[0,2:4], r_points[1,2:4])
+            colors = ['#FF0000A0']*(len(neighbors) + 1)
+            colors[-1] = '#80ff00'
+            sizes = [60]*(len(neighbors) + 1)
+            sizes[-1] = 150
+            if len(neighbors) > 0:
+                n_data = np.vstack([n.get_state()[0,0:2] for n in neighbors])
+            else:
+                n_data = np.array([0,0])
+            self.artists["scatter_single"].set(offsets=np.vstack((n_data, [0,0])), color=colors, sizes=sizes)
+            # Plot heading of estimated neighbors
+            if len(neighbors) > 0:
+                n_data2 = np.vstack([n.get_state()[3,0:2] for n in neighbors])
+                self.artists["quiver_n"] = self.ax[3].quiver(n_data[:,0],n_data[:,1],n_data2[:,0],n_data2[:,1],width=0.005, animated=True)
+            # Plot heading of the selected drone
+            scale = self.viewing_radius / 5.0
+            self.artists["arrow_single"] = self.ax[3].arrow(0, 0, swarm_states[selected_drone,-3], swarm_states[selected_drone,-2], width=scale*0.075, head_width=scale*0.3, head_length=scale*0.25, fc='g', ec='g')
+            # Plot swarm
+            self.artists["scatter_swarm"].set(offsets=[swarm_states[i,:2] - np.array([t_x,t_y]) for i in range(self._swarm_ref.count) if i != selected_drone])
+        except:
+            pass
         return self.artists.values()
     
     def stop(self):

swarm.py
@@ -19,11 +19,10 @@ class Swarm():
         if count == 1:
             self.members.append(Drone(init_pos=box[0:3]))
         else:
-            for _ in range(count):
-                x = box[0] + box[3]*(np.random.rand()-0.5)
-                y = box[1] + box[4]*(np.random.rand()-0.5)
-                z = box[2] + box[5]*(np.random.rand()-0.5)
-                self.members.append(Drone(init_pos=[x,y,z], init_angles=[0,0,0]))
+            box = np.array(box)
+            pos = np.random.uniform(box[0:3] - box[3:6]/2, box[0:3] + box[3:6]/2, size=(count,3))
+            for p in pos:
+                self.members.append(Drone(init_pos=p, init_angles=[0,0,0]))
         print("INITIALIZING SWARM: {0} drones within {1} box".format(count, box))
 
         # Intitialize optional parameters
@@ -33,7 +32,7 @@ class Swarm():
             self.noise = kwargs['noise']
         if 'algo_params' in kwargs:
             self.algo_params = kwargs['algo_params']
-        self.neighbors_metric = kwargs.get('neighbors_metric', {'computation':'None', 'metric': 'Eucledian', 'sampling': 1, 'metric_specific': {}})
+        self.neighbors_params = kwargs.get('neighbors_metric', {'computation':'None', 'metric': 'Eucledian', 'sampling': 1})
         
     def update(self, dt, new_acc):
         for m in self.members:
@@ -47,13 +46,16 @@ class Swarm():
         # Increment the update counter (used for different purposes, e.g. sampling the computation of the neighborhood metric)
         self.update_counter += 1
         # Compute each drone neighborhood
-        computation_method = self.neighbors_metric.get('computation', 'None')
-        if computation_method != 'None':
-            if (self.update_counter % self.neighbors_metric.get('sampling', 1)) == 0:
+        computation_method = self.neighbors_params.get('computation', 'None')
+        if computation_method == 'None':
+            # Clear all neighbors44
+            for m in self.members:
+                m.neighbors = []
+        else:
+            if (self.update_counter % self.neighbors_params.get('sampling', 1)) == 0:
                 # Only execute the computation every N steps
-                metric = self.neighbors_metric.get('metric', 'Eucledian')
-                metric_data = self.neighbors_metric.get('metric_specific', {})
-                self.compute_neighborhood(metric, metric_data, computation_method)
+                metric = self.neighbors_params.get('metric', 'Eucledian')
+                self.compute_neighborhood(metric, self.neighbors_params, computation_method)
 
     def set_cmd_velocity(self, v_ref):
         self.algo_params['v_ref'] = v_ref
@@ -61,8 +63,14 @@ class Swarm():
     def get_cmd_velocity(self):
         return self.algo_params.get('v_ref', np.zeros(3))
     
-    def update_neighbors_metric(self, new_params, apply_to_all):
-        self.neighbors_metric.update(new_params)
+    def update_neighbors_metric(self, new_params):
+        self.neighbors_params.update(new_params)
+
+    def get_neighbor_metric(self, metric, default_val):
+        if metric in self.neighbors_params:
+            return self.neighbors_params[metric]
+        else:
+            return default_val
 
     def set_cmd_ang_rates(self, rates):
         if not np.all(rates == self.ang_rates):