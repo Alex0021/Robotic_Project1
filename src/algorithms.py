latest fixes - timings plot sum = 1 - max angle computation for convez hull with only 2 points

src/algorithms.py
@@ -178,48 +178,65 @@ def convex_hull(drone, neighbors, params):
         params (dict): {'faces': 'adjacent' or 'visible', 'in_2d': True or False}
     """
     # Check if # neighbors is enough
-    if len(neighbors) < 3:
+    in_2d = params.get('in_2d', False)
+    if len(neighbors) < 2:
         print("WARNING :: Not enough neighbors to compute convex hull")
         return drone.get_heading()
-    in_2d = params.get('in_2d', False)
-    # Convex hull
-    points = np.array([n.get_abs_pos() for n in neighbors])
-    idx_drone = len(neighbors)
-    points = np.concatenate((points, [drone.pos]))
-    # Prepare standard options for qhull solver
-    qhull_options = '' if in_2d else 'tJ'
-    ndim = 2 if in_2d else 3
-    if in_2d:
-        points = points[:, :2]
-    if params.get('faces', 'adjacent') == 'adjacent':
-        hull = ConvexHull(points, qhull_options=f'Q{qhull_options}')
-        # Check if drone is in the convex hull
-        if idx_drone not in hull.vertices:
-            return drone.get_heading()
-        # Compute the normal of the adjacent faces
-        adj_idx = np.where(hull.simplices == idx_drone)[0]
-        normals = hull.equations[adj_idx, :ndim]
-        # Compute the viewing direction
-        viewing_dir = np.mean(normals, axis=0)
-    elif params.get('faces', 'adjacent') == 'visible':
-        # Edge case with 3 neighbors
-        if len(neighbors) == 3 and not in_2d:
-            centroid = np.mean(points[:3], axis=0)
-            normal = np.cross(points[1] - points[0], points[2] - points[0])
-            viewing_dir = -np.sign(np.dot(normal, centroid - points[-1]))*normal
-        else:
-            hull = ConvexHull(points, qhull_options=f'QG{idx_drone}{qhull_options}')
-            # Compute the normal of the visible faces
-            visible_idx = np.where(hull.good)[0]
-            # If no visible faces, return drone heading (change nothing)
-            if len(visible_idx) == 0:
-                #print("WARNING :: No visible faces")
+    elif len(neighbors) == 2:
+        # Do outter2 metric (max angle)
+        neighbors_pos = np.array([n.get_abs_pos() for n in neighbors])
+        dists = neighbors_pos - drone.pos
+        if in_2d:
+            dists = dists[:, :2]
+        # Compute dot product between all combination of neighbors
+        smaller = np.inf
+        smaller_indices = (0, 0)
+        for i in range(len(neighbors)):
+            for j in range(i+1, len(neighbors)):
+                d = np.dot(dists[i], dists[j])
+                if d < smaller:
+                    smaller = d
+                    smaller_indices = (i, j)
+        viewing_dir = -(dists[smaller_indices[0]] + dists[smaller_indices[1]]) / 2
+    else:
+        # Convex hull
+        points = np.array([n.get_abs_pos() for n in neighbors])
+        idx_drone = len(neighbors)
+        points = np.concatenate((points, [drone.pos]))
+        # Prepare standard options for qhull solver
+        qhull_options = '' if in_2d else 'tJ'
+        ndim = 2 if in_2d else 3
+        if in_2d:
+            points = points[:, :2]
+        if params.get('faces', 'adjacent') == 'adjacent':
+            hull = ConvexHull(points, qhull_options=f'Q{qhull_options}')
+            # Check if drone is in the convex hull
+            if idx_drone not in hull.vertices:
                 return drone.get_heading()
-            normals = hull.equations[visible_idx, :ndim]
+            # Compute the normal of the adjacent faces
+            adj_idx = np.where(hull.simplices == idx_drone)[0]
+            normals = hull.equations[adj_idx, :ndim]
             # Compute the viewing direction
             viewing_dir = np.mean(normals, axis=0)
-    else:
-        raise ValueError("Invalid value for 'faces' in params")
+        elif params.get('faces', 'adjacent') == 'visible':
+            # Edge case with 3 neighbors
+            if len(neighbors) == 3 and not in_2d:
+                centroid = np.mean(points[:3], axis=0)
+                normal = np.cross(points[1] - points[0], points[2] - points[0])
+                viewing_dir = -np.sign(np.dot(normal, centroid - points[-1]))*normal
+            else:
+                hull = ConvexHull(points, qhull_options=f'QG{idx_drone}{qhull_options}')
+                # Compute the normal of the visible faces
+                visible_idx = np.where(hull.good)[0]
+                # If no visible faces, return drone heading (change nothing)
+                if len(visible_idx) == 0:
+                    #print("WARNING :: No visible faces")
+                    return drone.get_heading()
+                normals = hull.equations[visible_idx, :ndim]
+                # Compute the viewing direction
+                viewing_dir = np.mean(normals, axis=0)
+        else:
+            raise ValueError("Invalid value for 'faces' in params")
     
     # Add z component to zero if in 2D
     if in_2d: