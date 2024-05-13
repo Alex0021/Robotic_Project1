d38289130a69ac302145e1651d16b9922b608188
Alex033 <alexandre-1.hebert@outlook.com>
2024-05-13 10:42:51 +0200

    changed toor folder for sim output files

--- GUI.py
+++ GUI.py
@@ -19,7 +19,7 @@ from src.tester import AutorunSim
 w,h = (1600,800)
 CONFIG_FILENAME = 'config/app_config.json'
 AUTORUN_FILENAME = 'config/sim_autorun.json'
-DATA_OUTPUT_FOLDER = 'output'
+DATA_OUTPUT_FOLDER = 'sim_results'
 FRONTEND_UPDATE_INTERVAL = 0.1
 TEST_COMPLETED_CHECK_INTERVAL = 0.5
 
